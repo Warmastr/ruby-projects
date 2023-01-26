8b47156a68091f037b19f9413efffdcad7f2c8a9
warmastr <jargyle80@yahoo.com>
2023-01-25 22:52:16 -0600

    Completed Project

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,8 +1,23 @@
-def stock_picker(stock_array)
-  # takes an array of stock prices, one price for each day. Need to return a pair of days, the best day to buy, and the best day to sell. 
+def stock_picker(prices)
+  min_price = prices[0] # initializing the minimum price that can exist
+  max_profit = 0
+  profit = 0
+  lowest_price = 0
+  highest_price = 0
+  result = [] # return the result in an ordered pair of indexes
 
-  # will need to iterate over the entire array and locate the lowest price that isn't the last day. Then, from that point, locate the highest price from the lowest point, to the end of the array. 
-
-  result = []
-  # use min and max methods to locate the lowest and highest stock prices and return the index of each. 
+  prices.each_with_index do |price, day|
+    if price < min_price && price != prices.last
+      min_price = price
+      lowest_price = day
+    end
+    profit = price - min_price
+    if profit > max_profit
+      highest_price = day
+      max_profit = profit
+    end
+  end
+  result = [lowest_price, highest_price]
 end
+
+print stock_picker([17,18,3,6,9,15,8,19,1,10,1]).inspect
